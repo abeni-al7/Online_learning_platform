3f511abe11a77f7521e7c4e2f077b8359cf05607
abeni-al7 <abenezeralebachew3@gmail.com>
2024-09-08 00:25:56 +0300

    Create UI for login and register pages

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def register():
     return render_template('register.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
