d2244c8ad8e2d4b6c1eadcd71e23fa701950a530
abeni-al7 <abenezeralebachew3@gmail.com>
2024-09-08 01:05:13 +0300

    Create UI for courses page

--- app.py
+++ app.py
@@ -14,5 +14,27 @@ def register():
 def login():
     return render_template('login.html')
 
+@app.route('/courses')
+def courses():
+    courses = [
+        {
+            'name': 'Python Programming',
+            'code': 'PY101',
+            'description': 'Learn Python Programming from scratch'
+        },
+        {
+            'name': 'Web Development',
+            'code': 'WD101',
+            'description': 'Learn Web Development from scratch'
+        },
+        {
+            'name': 'Data Science',
+            'code': 'DS101',
+            'description': 'Learn Data Science from scratch'
+        },
+    ]
+    role = 'student'
+    return render_template('courses.html', role=role, courses=courses)
+
 if __name__ == '__main__':
     app.run(debug=True)
