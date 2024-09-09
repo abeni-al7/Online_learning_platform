dcc994e58f9e781d588988774199c935080f65c9
abeni-al7 <abenezeralebachew3@gmail.com>
2024-09-10 01:41:45 +0300

    Setup Database and implement registration logic

--- app.py
+++ app.py
@@ -1,17 +1,65 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+db = SQLAlchemy()
+db.init_app(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+bcrypt = Bcrypt(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    from models import User
+    return User.query.get(int(id))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        name = request.form.get('name')
+        role = request.form.get('role')
+        password = request.form.get('password')
+        confirm = request.form.get('confirm-password')
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        if password != confirm:
+            flash('Passwords don\'t match')
+            print('Passwords don\'t match')
+        else:
+            try:
+                from models import User
+                new_user = User(email=email, password=hashed_password)
+                db.session.add(new_user)
+                db.session.commit()
+                print('Success')
+                return redirect(url_for('login'))
+            except Exception as e:
+                flash('Something went wrong. Please try again.')
+                print(e)
     return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        from models import User
+        user = User.query.filter_by(email=email).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('courses'))
+        else:
+            flash('Login unsuccessful. Please check your username and password.')
     return render_template('login.html')
 
 @app.route('/courses')
@@ -130,4 +178,6 @@ def profile():
     return render_template('profile.html', user=user, role=role)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
